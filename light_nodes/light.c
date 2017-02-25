86040f5d302c5700be7dc3eec054ea0cad7a071c
Jono Muller <jonomuller@gmail.com>
2017-02-25 20:11:47 +0000

    Attempt to obtain distance via RSSI Values recorded are quite varied, not accurate enough yet Add fields to data struct in protocol.h

--- light_nodes/light.c
+++ light_nodes/light.c
@@ -15,6 +15,8 @@
 #include <stdint.h>
 #include "hid_leds.h"
 #include "net/rime/timesynch.h"
+#include "../protocol.h"
+#include <math.h>
 
 /*---------------------------------------------------------------------------*/
 #define CC26XX_DEMO_LOOP_INTERVAL       (CLOCK_SECOND * 20)
@@ -40,22 +42,119 @@
 #define CC26XX_DEMO_SENSOR_4     &button_down_sensor
 #define CC26XX_DEMO_SENSOR_5     &button_select_sensor
 #endif
+
+#define SPEED_OF_LIGHT 299792458
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
+static int authority_level;
+static rtimer_clock_t offset;
+
+static void
+adjust_offset(rtimer_clock_t authoritative_time, rtimer_clock_t local_time)
+{
+  offset = authoritative_time - local_time;
+}
+
+
+// reverses a string 'str' of length 'len'
+void reverse(char *str, int len)
+{
+    int i=0, j=len-1, temp;
+    while (i<j)
+    {
+        temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+        i++; j--;
+    }
+}
+ 
+ // Converts a given integer x to string str[].  d is the number
+ // of digits required in output. If d is more than the number
+ // of digits in x, then 0s are added at the beginning.
+int intToStr(int x, char str[], int d)
+{
+    int i = 0;
+    while (x)
+    {
+        str[i++] = (x%10) + '0';
+        x = x/10;
+    }
+ 
+    // If number of digits required is more, then
+    // add 0s at the beginning
+    while (i < d)
+        str[i++] = '0';
+ 
+    reverse(str, i);
+    str[i] = '\0';
+    return i;
+}
+
+// Converts a floating point number to string.
+void ftoa(float n, char *res, int afterpoint)
+{
+    // Extract integer part
+    int ipart = (int)n;
+ 
+    // Extract floating part
+    float fpart = n - (float)ipart;
+ 
+    // convert integer part to string
+    int i = intToStr(ipart, res, 0);
+ 
+    // check for display option after point
+    if (afterpoint != 0)
+    {
+        res[i] = '.';  // add dot
+ 
+        // Get the value of fraction part upto given no.
+        // of points after dot. The third parameter is needed
+        // to handle cases like 233.007
+        fpart = fpart * pow(10, afterpoint);
+ 
+        intToStr((int)fpart, res + i + 1, afterpoint);
+    }
+}
 
 /*---------------------------------------------------------------------------*/
 static struct broadcast_conn broadcast;
 static void broadcast_recv(struct broadcast_conn *c, const linkaddr_t *from)
 {
-  rtimer_clock_t start_time = packetbuf_dataptr();
+  data_packet_header packet;
+
+  memcpy(&packet, packetbuf_dataptr(), sizeof(packet));
+
+  // clock sync method
+  if(packet.authority_level < authority_level) {
+  adjust_offset(packet.timestamp + packet.authority_offset,
+                packetbuf_attr(PACKETBUF_ATTR_TIMESTAMP));
+  timesynch_set_authority_level(packet.authority_level + 1);
+  }
+
+  int start_time = packet.timestamp;
   int end_time = (int) timesynch_time();
-  // int difference = end_time - start_time;
-  int difference = 0;
-  printf("start: %s, end: %d, difference: %d\n", start_time, end_time, difference);
-  leds_toggle(LEDS_RED);
-  printf("broadcast message received from %d.%d: '%s'\n",from->u8[0], from->u8[1],
-      (char *)packetbuf_dataptr());
+  int difference = end_time - start_time; // need to convert to seconds
+  int distance = SPEED_OF_LIGHT * difference;
+
+  // printf("start: %d, end: %d, difference: %d\n", start_time, end_time, difference);
+  // leds_toggle(LEDS_RED);
+  // printf("distance = %d\n", distance);
+
+
+  // rssi method
+  float rssi = (float) packetbuf_attr(PACKETBUF_ATTR_RSSI) - 65536;
+  float strength_1m = -50.0;
+  float n = 2.0;
+  float d = pow(10.0, ((rssi - strength_1m) / (-10.0 * n)));
+  
+  char rssi_string[20];
+  ftoa(rssi, rssi_string, 2);
+  char d_string[20];
+  ftoa(d, d_string, 2);
+
+  printf("rssi = %s, d = %s\n", rssi_string, d_string);
 }
 static const struct broadcast_callbacks broadcast_call = {broadcast_recv};
 /*---------------------------------------------------------------------------*/
@@ -68,7 +167,9 @@ AUTOSTART_PROCESSES(&example_process);
 PROCESS_THREAD(example_process, ev, data)
 {
   static struct etimer et;
+  data_packet_header packet;
   timesynch_init();
+  authority_level = 0;
 
   PROCESS_EXITHANDLER(broadcast_close(&broadcast));
 
@@ -77,18 +178,21 @@ PROCESS_THREAD(example_process, ev, data)
   broadcast_open(&broadcast, 135, &broadcast_call);
   hid_on();
   while(1) {
-    etimer_set(&et, CLOCK_SECOND * 4 + random_rand() % (CLOCK_SECOND * 4));
+    etimer_set(&et, CLOCK_SECOND);
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
     leds_toggle(LEDS_ALL);
 
-    rtimer_clock_t *time = timesynch_time();
-    printf("%p", time);
-    packetbuf_copyfrom(time, sizeof(time));
+    // packet.timestamp = timesynch_time();
+    packet.authority_offset = offset;
+    packet.authority_level = authority_level;
+    packet.timestamp = timesynch_time();
+
+    packetbuf_copyfrom(&packet, sizeof(packet));
     packetbuf_set_attr(PACKETBUF_ATTR_PACKET_TYPE,
                        PACKETBUF_ATTR_PACKET_TYPE_TIMESTAMP);
     broadcast_send(&broadcast);
 
-    printf("broadcast message sent\n");
+    // printf("broadcast message sent\n");
     leds_toggle(LEDS_ALL);
 
     }
--- protocol.h
+++ protocol.h
@@ -7,6 +7,9 @@ typedef struct {
   int system_code;
   int source_node_type;
   int packet_type;
+  uint8_t authority_level;
+  uint16_t authority_offset;
+  uint16_t timestamp;
 } data_packet_header;
 
 typedef struct {
