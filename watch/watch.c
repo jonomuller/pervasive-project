204a630a299872acc212acc78f5b060564cb13eb
Thomas Szyszko <thomas.szyszko13@imperial.ac.uk>
2017-02-26 21:46:16 +0000

    seperated watch channel from internode channel

--- watch/watch.c
+++ watch/watch.c
@@ -61,6 +61,59 @@ PROCESS_THREAD(watch_button_process, ev, data)
   static struct mmem mmem;
   PROCESS_EXITHANDLER(broadcast_close(&broadcast));
   PROCESS_BEGIN();
+  broadcast_open(&broadcast, WATCH_BROADCAST_CHANNEL, &broadcast_call);
+  mmem_init();
+  while(1) {
+    PROCESS_WAIT_EVENT_UNTIL(ev == sensors_event);
+    if (data == CC26XX_DEMO_SENSOR_1)  {
+      // Light switch button press
+      packetbuf_clear();
+      switch_pos = !switch_pos;
+      data_packet_header header;
+      header.system_code = SYSTEM_CODE;
+      header.source_node_type = 0;
+      header.packet_type = switch_pos? ON_PACKET: OFF_PACKET;
+      packetbuf_copyfrom(&header,sizeof(data_packet_header));
+      broadcast_send(&broadcast);
+    } else if (data == CC26XX_DEMO_SENSOR_2)  {
+      // Switch colour press
+      if (current_colour < COLOUR_CODE_BLUE) {
+        current_colour++;
+      } else current_colour = COLOUR_CODE_WHITE;
+      packetbuf_clear();
+      switch_pos = !switch_pos;
+      data_packet_header header;
+      header.system_code = SYSTEM_CODE;
+      header.source_node_type = 0;
+      header.packet_type = LIGHT_SETTINGS_PACKET;
+      light_settings_packet settings;
+      settings.light_colour = current_colour;
+      settings.light_intensity = current_intensity;
+
+      int packet_size = sizeof(data_packet_header)
+          + sizeof(light_settings_packet);
+      if(mmem_alloc(&mmem, packet_size) == 0) {
+        printf("memory allocation failed\n");
+      } else {
+        char * packet = (char *) MMEM_PTR(&mmem);
+        memcpy(packet,&header,sizeof(data_packet_header));
+        memcpy(packet+sizeof(data_packet_header),&settings,
+            sizeof(light_settings_packet));
+        void * void_ptr = (void *) packet;
+        packetbuf_copyfrom(void_ptr,packet_size);
+        broadcast_send(&broadcast);
+      }
+    }
+
+  }
+  PROCESS_END();
+}
+
+PROCESS_THREAD(watch_announce_process, ev, data)
+{
+  static struct mmem mmem;
+  PROCESS_EXITHANDLER(broadcast_close(&broadcast));
+  PROCESS_BEGIN();
   broadcast_open(&broadcast, BROADCAST_CHANNEL, &broadcast_call);
   mmem_init();
   while(1) {
